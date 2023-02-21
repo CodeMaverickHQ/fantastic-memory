07d6fe7460e7088b1ca38f5f92dcf45028103ddc
jeffvos <jeff.vos@aol.com>
2023-02-21 17:22:13 +0100

    starting sql

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -19,7 +19,7 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config['OPENAPI_SWAGGER_UI_PATH'] = "swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI", "sqlite:///data.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URI", "sqlite:///data.db")
 
     api = Api(app)
     api.register_blueprint(ItemBlueprint)
