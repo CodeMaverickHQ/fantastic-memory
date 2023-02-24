c47781cfc93e402d34e693633ce88d5c62fb70e2
jeffvos <jeff.vos@aol.com>
2023-02-24 05:58:51 +0100

    fixed wrong itemtags model

--- models/item_tags.py
+++ models/item_tags.py
@@ -5,6 +5,3 @@ class ItemTags(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey("items.id"))
     tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"))
-
-    store = db.relationship("StoreModel", back_populates="tags")
-    items = db.relationship("ItemModel", back_populates="tags", secondary="items_tags")
