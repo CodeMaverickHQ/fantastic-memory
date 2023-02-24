70763b7c081d5c5890ac39d238cb555f9cb5400a
jeffvos <jeff.vos@aol.com>
2023-02-24 05:47:33 +0100

    changed tag vars input to int

--- resources/tag.py
+++ resources/tag.py
@@ -8,7 +8,7 @@ from schemas import TagSchema, TagandItemSchema
 
 blp = Blueprint("Tags", "tags", description="operations on tags")
 
-@blp.route("/store/<string:store_id>/tag")
+@blp.route("/store/<int:store_id>/tag")
 class TagsInStore(MethodView):
     @blp.response(200, TagSchema(many=True))
     def get(self, store_id):
@@ -31,7 +31,7 @@ class TagsInStore(MethodView):
             )
         return tag
     
-@blp.route("/item/<string:item_id>/tag/<string:tag_id>")
+@blp.route("/item/<int:item_id>/tag/<int:tag_id>")
 class LinkTagsToItem(MethodView):
     def post(self, item_id, tag_id):
         item = ItemModel.query.get_or_404(item_id)
@@ -58,7 +58,7 @@ class LinkTagsToItem(MethodView):
             abort(500, message="some error occurred")
         return {"message":"Item removed from tag", "item":item, "tag":tag}        
     
-@blp.route("/tag/<string:tag_id>")
+@blp.route("/tag/<int:tag_id>")
 class Tag(MethodView):
     @blp.response(200, TagSchema)
     def get(self, tag_id):
