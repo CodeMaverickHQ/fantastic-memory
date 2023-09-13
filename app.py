b7e73a36ed30386341da9d3a2b02cd1da8636010
jeffvos <jeff.vos@aol.com>
2023-09-13 12:09:03 +0200

    jwt

--- app.py
+++ app.py
@@ -32,18 +32,28 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "jojo"
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        pass
+
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return(
-            jsonify({"message":"the tokens has expired", "error": "token_expired"}),
+            jsonify(
+                {
+                    "message":"the tokens has expired", 
+                    "error": "token_expired"
+                }),
             401
         )
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return (
             jsonify(
-                {"message":"Signature verification failed", "error":"invalid_token"}
-            ),
+                {
+                    "message":"Signature verification failed", 
+                    "error":"invalid_token"
+                }),
             401
         )
     @jwt.unauthorized_loader
