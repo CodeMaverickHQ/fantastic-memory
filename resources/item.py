a7a94b4433e98e5d5aa1f655b20f45928af7a2a5
jeffvos <jeff.vos@aol.com>
2023-09-13 09:35:20 +0200

    adding jwt req

--- resources/item.py
+++ resources/item.py
@@ -12,11 +12,13 @@ blp = Blueprint("Items", __name__, description="Operations on Items")
 
 @blp.route("/item/<int:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
-
+    
+    @jwt_required()
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemUpdateSchema)
     def put(self, item_data, item_id):
@@ -31,7 +33,8 @@ class Item(MethodView):
         db.session.commit()
 
         return item
-
+    
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
