5d693124f5f930d696030dd895952e952db4a6e4
jeffvos <jeff.vos@aol.com>
2023-09-13 12:37:13 +0200

    db migration

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import secrets
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -28,7 +29,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URI", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
     # secrets.SystemRandom().getrandbits(128)
     app.config["JWT_SECRET_KEY"] = "jojo"
@@ -98,9 +99,9 @@ def create_app(db_url=None):
             401
         )
 
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
+    # @app.before_first_request
+    # # def create_tables():
+    # #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
